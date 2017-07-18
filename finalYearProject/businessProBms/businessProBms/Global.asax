c0fd8dda16f6d8519a3f11d34aaae5188ab66c73
hafizSiddiq7675 <hafiz.siddiq2k17@gmail.com>
2017-07-18 12:44:00 -0700

    Start 18/07/2017

---
+++ finalYearProject/businessProBms/businessProBms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="businessProBms.MvcApplication" Language="C#" %>
---
+++ finalYearProject/businessProBms/businessProBms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(businessProBms.Startup))]
+namespace businessProBms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
