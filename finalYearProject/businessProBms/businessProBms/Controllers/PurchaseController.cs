f31600257589b56126421ffaedd3a4f6861bdb39
hafizSiddiq7675 <hafiz.siddiq2k17@gmail.com>
2017-07-24 11:34:45 -0700

    updated on 24/07/2017
    Working on Master Detail form

--- finalYearProject/businessProBms/businessProBms/Controllers/PurchaseController.cs
+++ finalYearProject/businessProBms/businessProBms/Controllers/PurchaseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using businessProBms.Models;
 
 namespace businessProBms.Controllers
 {
@@ -10,9 +11,36 @@ namespace businessProBms.Controllers
     {
         //
         // GET: /Purchase/
+        businessProBmsEntities db = new businessProBmsEntities();
         public ActionResult Purchases()
         {
+            ViewBag.vendors = db.Vendors.ToList();
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Purchases([Bind(Include="purchaseId,purchaseDate,vendorCode,vendorName")] Purchase purchase)
+        {
+            if(ModelState.IsValid)
+            {
+                var pur = db.Purchases.SingleOrDefault(m => m.purchaseId == purchase.purchaseId);
+                    if(pur==null)
+                    {
+                        db.Purchases.Add(purchase);
+                    }
+                    else
+                    {
+                        var purDb=db.Purchases.Single(m=>m.purchaseId==pur.purchaseId);
+                        purDb.purchaseId=purchase.purchaseId;
+                        purDb.purchaseDate=purchase.purchaseDate;
+                        purDb.vendorCode=purchase.vendorCode;
+                        purDb.vendorName=purchase.vendorName;
+                    }
+                db.SaveChanges();
+                return RedirectToAction("Purchases");
+            }
+            ViewBag.vendors=db.Vendors.ToList();
+            return View(purchase);
         }
 	}
 }
